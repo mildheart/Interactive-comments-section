3e456b862760e5656b558718e6fa20c0e6bddf9e
dolphy <uwanduchibuike@gmail.com>
2023-05-15 10:26:40 +0100

    html,css and images files

---
+++ dist/main.js
@@ -0,0 +1,117 @@
+const comment = {
+    "currentUser": {
+      "image": { 
+        "png": "./images/avatars/image-juliusomo.png",
+        "webp": "./images/avatars/image-juliusomo.webp"
+      },
+      "username": "juliusomo"
+    },
+    "comments": [
+      {
+        "id": 1,
+        "content": "Impressive! Though it seems the drag feature could be improved. But overall it looks incredible. You've nailed the design and the responsiveness at various breakpoints works really well.",
+        "createdAt": "1 month ago",
+        "score": 12,
+        "user": {
+          "image": { 
+            "png": "./images/avatars/image-amyrobson.png",
+            "webp": "./images/avatars/image-amyrobson.webp"
+          },
+          "username": "amyrobson"
+        },
+        "replies": []
+      },
+      {
+        "id": 2,
+        "content": "Woah, your project looks awesome! How long have you been coding for? I'm still new, but think I want to dive into React as well soon. Perhaps you can give me an insight on where I can learn React? Thanks!",
+        "createdAt": "2 weeks ago",
+        "score": 5,
+        "user": {
+          "image": { 
+            "png": "./images/avatars/image-maxblagun.png",
+            "webp": "./images/avatars/image-maxblagun.webp"
+          },
+          "username": "maxblagun"
+        },
+        "replies": [
+          {
+            "id": 3,
+            "content": "If you're still new, I'd recommend focusing on the fundamentals of HTML, CSS, and JS before considering React. It's very tempting to jump ahead but lay a solid foundation first.",
+            "createdAt": "1 week ago",
+            "score": 4,
+            "replyingTo": "maxblagun",
+            "user": {
+              "image": { 
+                "png": "./images/avatars/image-ramsesmiron.png",
+                "webp": "./images/avatars/image-ramsesmiron.webp"
+              },
+              "username": "ramsesmiron"
+            }
+          },
+          {
+            "id": 4,
+            "content": "I couldn't agree more with this. Everything moves so fast and it always seems like everyone knows the newest library/framework. But the fundamentals are what stay constant.",
+            "createdAt": "2 days ago",
+            "score": 2,
+            "replyingTo": "ramsesmiron",
+            "user": {
+              "image": { 
+                "png": "./images/avatars/image-juliusomo.png",
+                "webp": "./images/avatars/image-juliusomo.webp"
+              },
+              "username": "juliusomo"
+            }
+          }
+        ]
+      }
+    ]
+  }
+
+  
+
+  let section_container = document.querySelector('.main_containter');
+
+  function settingUp(){
+
+    section_container.innerHTML = getForm(comment.comments[0].score,comment.comments[0].username,comment.comments[0].createdAt,comment.comments[0].content);
+
+    
+
+  }
+
+  window.addEventListener('DOMContentLoaded',function(){
+    settingUp();
+  })
+
+  function getForm(object_score,Names,period,content){
+    return `<section class="Comments">
+    <div class="individual_comment">
+        <div class="inner_control">
+           <button><img src="/images/icon-minus.svg" alt="Minus button"></button>
+           <p class= "score">${object_score}</p>
+           <button><img src="/images/icon-plus.svg" alt=""></button>
+        </div>
+        <div class="out_container">
+            <div class="details">
+                <div class="header">
+                    <img src="/images/avatars/image-amyrobson.png" alt="">
+                    <p class="Name">${Names}</p>
+                    <p class="you"></p>
+                    <p class="time_range">${period}</p>
+                </div>
+                <div class="reply_comment">
+                    <img src="/images/icon-reply.svg" alt="">
+                    <p class="reply_button">
+                        Reply
+                    </p>
+                </div>
+            </div>
+            <p class="main_info">${content}</p>
+      </div> 
+    </div>
+    <div class="replies"></div>
+  </section>`;
+  }
+ 
+
+
